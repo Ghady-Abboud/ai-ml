ef6576b35cfea19fdccf5aa01ac55b6bf31def1c
Ghady Abboud <abboudghady@gmail.com>
2025-11-22 14:25:18 -0500

    Ridge Func + change file type

--- lasso-ridge-comp/src/ridge.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class RidgeRegression:
-  def __init__(self, data, learning_rate = 0.01):
-    self.data = data
-    self.learning_rate = learning_rate
-  
-  def fit(self, X, Y):
-    self.m, self.n = X.shape
